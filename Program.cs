785c7d8b46e6272bfbcc71bb72305ba7d812e30f
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-11-13 11:31:22 +0100

    Type testing with is and as

--- Program.cs
+++ Program.cs
@@ -17,7 +17,24 @@ namespace csharp_oop_1_demo
 
             foreach (var p in people)
             {
-                Console.WriteLine($"{p.FirstName} {p.LastName}"); 
+                Console.WriteLine($"{p.FirstName} {p.LastName}");
+
+                Console.WriteLine(p.GetType());
+
+                if (p.GetType() == typeof(Employee))
+                {
+                    Employee e1 = (Employee)p;
+                    Console.WriteLine(e1.Salary);
+                }
+
+                if (p is Employee e)
+                {
+                    Console.WriteLine(e.Salary);
+                }
+
+                // Employee e2 = (Employee)p
+                Employee e2 = p as Employee;
+                Console.WriteLine(e2);
             }
         }
     }
