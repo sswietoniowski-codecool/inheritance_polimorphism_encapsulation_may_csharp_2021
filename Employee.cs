a032bb13f62442d9530b126231925e5cacf7940a
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-11-13 10:25:40 +0100

    Created parameterless constructor in Person and Employee

--- Employee.cs
+++ Employee.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace csharp_oop_1_demo
 {
     class Employee : Person
     {
+        public Employee()
+        {
+            Salary = 0;
+            Console.WriteLine("Wywołano konstruktor bezparamtrowy z Employee");
+        }
+
         public decimal Salary { get; set; }
     }
 }
--- Person.cs
+++ Person.cs
@@ -12,6 +12,20 @@ namespace csharp_oop_1_demo
         public string LastName { get; set; }
         public int YearOfBirth { get; set; }
 
+        public Person(string firstName, string lastName, int yearOfBirth)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            YearOfBirth = yearOfBirth;
+
+            Console.WriteLine("Wywołano konstruktor z parametrami z Person");
+        }
+
+        public Person() : this("NIEZNANY", "NIEZNANY", 1900)
+        {
+            Console.WriteLine("Wywołano konstruktor bezparametrowy z Person");
+        }
+
         public void WhoAmI()
         {
             //Console.WriteLine("My Name is " + LastName + ". " + FirstName + " " + LastName + ".");
