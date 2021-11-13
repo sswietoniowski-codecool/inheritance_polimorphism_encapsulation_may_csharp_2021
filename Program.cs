66b8b95b5e7565b2266bef0f0221af50cdf3fce4
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-11-13 11:04:15 +0100

    Assigning derived class object to base class variable

--- Program.cs
+++ Program.cs
@@ -6,16 +6,25 @@ namespace csharp_oop_1_demo
     {
         static void Main(string[] args)
         {
-            Employee employee = new Employee()
-            {
-                FirstName = "James",
-                LastName = "Bond",
-                YearOfBirth = 1939
-            };
-            employee.WhoAmI();
-            Console.WriteLine(employee.IsAdult());
+            //Employee employee = new Employee()
+            //{
+            //    FirstName = "James",
+            //    LastName = "Bond",
+            //    YearOfBirth = 1939
+            //};
+            //employee.WhoAmI();
+            //Console.WriteLine(employee.IsAdult());
 
-            Employee secondEmployee = new Employee("Jan", "Kowalski", 1981, 5000);
+            //Employee secondEmployee = new Employee("Jan", "Kowalski", 1981, 5000);
+
+            //Employee employee = new Employee("Jan", "Kowalski", 1981, 5000);
+            //Console.WriteLine(employee.Salary);
+            //Person person = new Employee("Anna", "Nowak", 1982, 5000);
+            //Person person2 = new Person("Anna", "Nowak", 1982);
+            //Console.WriteLine(person ==person2);
+            ////Console.WriteLine(person.Salary);
+            //Console.WriteLine(person.LastName);
+            //Employee employee = new Person("Anna", "Nowak", 1982);
         }
     }
 }
