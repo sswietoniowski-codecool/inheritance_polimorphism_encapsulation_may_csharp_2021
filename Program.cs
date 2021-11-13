870e99f911e154a8f5bc08af122544e2c3ecbe46
Therion95 <piotrpawelczyk95@gmail.com>
2021-11-13 15:53:36 +0100

    Created class orderitem

--- Program.cs
+++ Program.cs
@@ -7,14 +7,7 @@ namespace csharp_oop_1_demo
     {
         static void Main(string[] args)
         {
-            Manager manager = new Manager("Jan", "Kowalski", 1990, 5000)
-            {
-                RoomNumberNewWay = 5
-            };
-            Console.WriteLine(manager.RoomNumberNewWay);
-            //manager.RoomNumberNewWay = 6;
-            Console.WriteLine(manager.RoomNumber);
-            //manager.RoomNumber = 5;
+            
         }
     }
 }
