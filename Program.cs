0b110cc0e2aa23974a7a395c7ed02ab499b81490
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-11-13 11:37:34 +0100

    Base class as a method parameter

--- Program.cs
+++ Program.cs
@@ -14,27 +14,41 @@ namespace csharp_oop_1_demo
 
             people.Add(employee);
             people.Add(person);
+            people.Add(new Person("Adam", "Nowak", 2015));
 
             foreach (var p in people)
             {
-                Console.WriteLine($"{p.FirstName} {p.LastName}");
+                //Console.WriteLine($"{p.FirstName} {p.LastName}");
 
-                Console.WriteLine(p.GetType());
+                //Console.WriteLine(p.GetType());
 
-                if (p.GetType() == typeof(Employee))
-                {
-                    Employee e1 = (Employee)p;
-                    Console.WriteLine(e1.Salary);
-                }
+                //if (p.GetType() == typeof(Employee))
+                //{
+                //    Employee e1 = (Employee)p;
+                //    Console.WriteLine(e1.Salary);
+                //}
 
-                if (p is Employee e)
-                {
-                    Console.WriteLine(e.Salary);
-                }
+                //if (p is Employee e)
+                //{
+                //    Console.WriteLine(e.Salary);
+                //}
 
-                // Employee e2 = (Employee)p
-                Employee e2 = p as Employee;
-                Console.WriteLine(e2);
+                //// Employee e2 = (Employee)p
+                //Employee e2 = p as Employee;
+                //Console.WriteLine(e2);
+                PrintHello(p);
+            }
+        }
+
+        private static void PrintHello(Person person)
+        {
+            if (person.IsAdult())
+            {
+                Console.WriteLine("Witaj w sklepie monopolowym");
+            }
+            else
+            {
+                Console.WriteLine("Witaj w sklepie z zabawkami");
             }
         }
     }
